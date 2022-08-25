d9c456927ec1b74c67c65cbbe114c70d8a0446b1
WillTDP <77252197+WillTDP@users.noreply.github.com>
2022-08-25 02:44:34 +0200

    repaired something that broke

--- reset-password.php
+++ reset-password.php
@@ -13,32 +13,32 @@ require_once "server.php";
  
 // Define variables and initialize with empty values
 $new_password = $confirm_password = "";
-$new_password_err = $confirm_password_err = "";
+$new_password_error = $confirm_password_error = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
     // Validate new password
     if(empty(trim($_POST["new_password"]))){
-        $new_password_err = "Please enter the new password.";     
+        $new_password_error = "Please enter the new password.";     
     } elseif(strlen(trim($_POST["new_password"])) < 6){
-        $new_password_err = "Password must have atleast 6 characters.";
+        $new_password_error = "Password must have atleast 6 characters.";
     } else{
         $new_password = trim($_POST["new_password"]);
     }
     
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm the password.";
+        $confirm_password_error = "Please confirm the password.";
     } else{
         $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($new_password_err) && ($new_password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
+        if(empty($new_password_error) && ($new_password != $confirm_password)){
+            $confirm_password_error = "Password did not match.";
         }
     }
         
     // Check input errors before updating the database
-    if(empty($new_password_err) && empty($confirm_password_err)){
+    if(empty($new_password_error) && empty($confirm_password_error)){
         // Prepare an update statement
         $sql = "UPDATE users SET password = ? WHERE id = ?";
         
